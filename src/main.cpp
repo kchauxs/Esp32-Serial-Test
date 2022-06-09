6c21d94482713bbd9e61fe5b3556e542239a7c52
Kevin Chaux Sandino <k55chasacch@gmail.com>
2022-06-09 00:58:58 -0500

    update code

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,25 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
-#define SENDER_TXD_PIN 12
-#define RECEIVER_RXD_PIN 13
+#define SENDER_TXD_PIN 12   // 13
+#define RECEIVER_RXD_PIN 13 // 12
 
-HardwareSerial serialSend(1);
-DynamicJsonDocument jsonSerialReceived(2048);
+const int device = 1; // 2
+const int intervalSerial = 100;
+unsigned long packages = 0;
+unsigned long previousSerialMillis = 0;
+
 String serialData;
+HardwareSerial serialSend(1);
+DynamicJsonDocument jsonSerialReceived(1024);
 
+bool readSerail();
+void receiveDataBySerial();
+void sendDataBySerial();
 bool validateDataSerialReceived(String serialData);
 bool validateJsonFromReceivedDataSerial(String serialData);
 
-void setup()
-{
-  Serial.begin(115200);
-  serialSend.begin(115200, SERIAL_8N1, SENDER_TXD_PIN, RECEIVER_RXD_PIN);
-  delay(200);
-  Serial.println("");
-  Serial.println("Received divice!");
-}
-
-void loop()
+bool readSerail()
 {
   bool isReceivedBySerial = false;
 
@@ -37,17 +36,36 @@ void loop()
     }
   }
 
-  if (isReceivedBySerial)
-  {
+  return isReceivedBySerial;
+}
 
+void receiveDataBySerial()
+{
+  serialData = "";
+
+  if (readSerail())
+  {
     if (!validateDataSerialReceived(serialData))
+      return;
+
+    if (!validateJsonFromReceivedDataSerial(serialData))
     {
-      serialData = "";
+      jsonSerialReceived.clear();
+      digitalWrite(BUILTIN_LED, HIGH);
+      delay(10000);
+      digitalWrite(BUILTIN_LED, LOW);
       return;
     }
-    Serial.println("Info: " + serialData);
+
+    String message = jsonSerialReceived["message"];
+    String packages = jsonSerialReceived["packages"];
+    String device = jsonSerialReceived["device"];
+
     Serial.println("");
-    serialData = "";
+    Serial.println("Info: " + serialData);
+    Serial.println("Info: message => " + message);
+    Serial.println("Info: packages => " + packages);
+    Serial.println("Info: device => " + device);
   }
 }
 
@@ -60,4 +78,61 @@ bool validateDataSerialReceived(String serialData)
     return false;
 
   return true;
+}
+
+bool validateJsonFromReceivedDataSerial(String serialData)
+{
+  deserializeJson(jsonSerialReceived, serialData);
+
+  if (jsonSerialReceived["message"].isNull())
+    return false;
+
+  if (jsonSerialReceived["packages"].isNull())
+    return false;
+
+  if (jsonSerialReceived["device"].isNull())
+    return false;
+
+  if (jsonSerialReceived["device"] == device)
+    return false;
+
+  return true;
+}
+
+void sendDataBySerial()
+{
+  long currentmillis = millis();
+
+  if ((currentmillis - previousSerialMillis) > intervalSerial)
+  {
+    packages++;
+    String toSend = "";
+
+    DynamicJsonDocument jsonSerialSend(2048);
+    jsonSerialSend["message"] = "dummy message";
+    jsonSerialSend["packages"] = packages;
+    jsonSerialSend["device"] = device;
+    serializeJson(jsonSerialSend, toSend);
+
+    toSend += '~';
+    serialSend.print(toSend);
+    previousSerialMillis = currentmillis;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  serialSend.begin(115200, SERIAL_8N1, SENDER_TXD_PIN, RECEIVER_RXD_PIN);
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(200);
+  Serial.println("Info: Device: " + String(device));
+  Serial.println("");
+}
+
+void loop()
+{
+  receiveDataBySerial();
+  sendDataBySerial();
 }
